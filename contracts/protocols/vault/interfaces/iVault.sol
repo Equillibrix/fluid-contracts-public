649d9ccf7b21dedc1c3c498e6f55507cb0c0d0d2
cryptolappi <o.trenkin@ya.ru>
2024-12-05 15:57:28 +0300

    made pragma less strict for interfaces

--- contracts/protocols/vault/interfaces/iVault.sol
+++ contracts/protocols/vault/interfaces/iVault.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.21;
+pragma solidity ^0.8.21;
 
 /// @notice common Fluid vaults interface, some methods only available for vaults > T1 (type, simulateLiquidate, rebalance is different)
 interface IFluidVault {
--- contracts/protocols/vault/interfaces/iVaultT1.sol
+++ contracts/protocols/vault/interfaces/iVaultT1.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.21;
+pragma solidity ^0.8.21;
 
 interface IFluidVaultT1 {
     /// @notice returns the vault id
